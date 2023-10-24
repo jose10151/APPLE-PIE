160e96e8b42448298d97267d2f0a1867526ca888
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-10-24 11:41:07 -0400

    DONE

--- APPLE PIE/ViewController.swift
+++ APPLE PIE/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+     
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
 }
+
+
+
+
 
