c07d25fbd8a371a2fc57c7f6488e5e3bafc6e9ee
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-10-25 14:00:48 -0400

    DONE

---
+++ APPLE PIE/Game.swift
@@ -0,0 +1,51 @@
+//
+//  Game.swift
+//  APPLE PIE
+//
+//  Created by 4d on 10/25/23.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+    var guessedWord = ""
+    for letter in word {
+    if guessedLetters.contains(letter) {
+    guessedWord += "\(letter)"
+    }
+    else {
+    guessedWord += "_"
+     }
+    }
+    return guessedWord
+     }
+    
+    mutating func playerGuessed(letter: Character) {
+    guessedLetters.append(letter)
+    if !word.contains(letter) {
+    incorrectMovesRemaining -= 1
+    }
+        }
+    
+    
+
+    
+    }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
